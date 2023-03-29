2b72ab4881653c2c2b93a48c1c0241a5417056cc
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-29 17:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
-
+const output=document.getElementById("output")
+async function fetchData(){
+	const res=await fetch(url);
+	const data= await res.json();
+	output.innerHTML=JSON.stringify(data);
+}
 //your JS code here. If required.
